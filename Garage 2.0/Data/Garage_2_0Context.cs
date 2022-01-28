dc8713c02437bbb9cb932beee40226d95b85295c
Judy-P-Eldh <pernilla.eldh@gmail.com>
2022-01-28 15:15:32 +0100

    Model, Controller

---
+++ Garage 2.0/Data/Garage_2_0Context.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Garage_2._0.Models.Entities;
+
+namespace Garage_2._0.Data
+{
+    public class Garage_2_0Context : DbContext
+    {
+        public Garage_2_0Context (DbContextOptions<Garage_2_0Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Garage_2._0.Models.Entities.Vehicle> Vehicle { get; set; }
+    }
+}
---
+++ Garage 2.0/Migrations/20220128140837_Init.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garage_2._0.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Wheels = table.Column<int>(type: "int", nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VehicleType = table.Column<int>(type: "int", nullable: false),
+                    CheckIn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CheckOut = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
