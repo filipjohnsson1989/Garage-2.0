7364a69917253f4fd6a4a9465e7790b1a204301e
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-02-03 15:44:24 +0100

    Add more functionality for DatesDiff

--- Garage 2.0/Common/Util.cs
+++ Garage 2.0/Common/Util.cs
@@ -42,14 +42,26 @@ namespace Garage_2._0.Common
         public static string ParkingTimeString(DateTime checkIn, DateTime checkOut)
         {
             TimeSpan timeSpan = checkOut - checkIn;
+            return ParkingTimeString(timeSpan);
+        }
+
+        /// <summary>
+        /// Creates a string representing the time a vehicle has been parked
+        /// </summary>
+        /// <param name="datesDiff">Differentiate of check in and checkout</param>
+        /// <returns>Returns string representantion of the parked time.
+        /// <example>45 m, 1 d 1 h 45 m</example></returns>
+        public static string ParkingTimeString(TimeSpan datesDiff)
+        {
             string timeString = "";
 
-            if (timeSpan.Days != 0) timeString += timeSpan.Days.ToString() + " d ";
-            if (timeSpan.Hours != 0) timeString += timeSpan.Hours.ToString() + " t ";
-            if (timeSpan.Minutes != 0) timeString += timeSpan.Minutes.ToString() + " m ";
+            if (datesDiff.Days != 0) timeString += datesDiff.Days.ToString() + " d ";
+            if (datesDiff.Hours != 0) timeString += datesDiff.Hours.ToString() + " t ";
+            if (datesDiff.Minutes != 0) timeString += datesDiff.Minutes.ToString() + " m ";
             //return timeSpan.Days + " " + timeSpan.Hours + ":" + timeSpan.Minutes + " " + String.Format(" {0:C2}", (timeSpan.TotalMinutes * 10 / 60));
             return timeString;
         }
+
 
         /// <summary>
         /// Calculates the cost of a parked vehicle.
@@ -63,8 +75,34 @@ namespace Garage_2._0.Common
             TimeSpan timeSpan = checkOut - checkIn;
 
             //Round down to full minutes
-            return Math.Round(Math.Round(timeSpan.TotalMinutes, 0, MidpointRounding.ToZero) * hourlyCost / 60.0, 2);
+            return ParkingTimeCost(timeSpan, hourlyCost);
             
+        }
+
+        /// <summary>
+        /// Calculates the cost of a parked vehicle.
+        /// </summary>
+        /// <param name="datesDiff">Differentiate of check in and checkout</param>
+        /// <param name="hourlyCost">The hourly cost</param>
+        /// <returns>Returns the calculated cost rounded to two decimal</returns>
+        public static double ParkingTimeCost(TimeSpan datesDiff, double hourlyCost)
+        {
+            //Round down to full minutes
+            return ParkingTimeCost(datesDiff.TotalMinutes, hourlyCost);
+
+        }
+
+        /// <summary>
+        /// Calculates the cost of a parked vehicle.
+        /// </summary>
+        /// <param name="TotalMinutes">Differentiate of check in and checkout in number of minutes</param>
+        /// <param name="hourlyCost">The hourly cost</param>
+        /// <returns>Returns the calculated cost rounded to two decimal</returns>
+        public static double ParkingTimeCost(double TotalMinutes, double hourlyCost)
+        {
+            //Round down to full minutes
+            return Math.Round(Math.Round(TotalMinutes, 0, MidpointRounding.ToZero) * hourlyCost / 60.0, 2);
+
         }
     }
 }
